8a14f5f18334b13553ad5de0893e6ee52938b0bf
Thomas Kirst <thomas.kirst@neuefische.de>
2021-03-21 15:45:20 +0100

    Added test for <ProductForm /> component. Configured test db when app is ran in test environment.

---
+++ client/tests/e2e/addProductForm.spec.js
@@ -0,0 +1,49 @@
+/// <reference types="Cypress" />
+
+describe('<ProductForm />', () => {
+  beforeEach(() => {
+    cy.viewport('ipad-2');
+    cy.visit('/');
+    cy.get('[href="/products"]').first().click();
+    cy.get('[href="/products/add-product"').click();
+  });
+  it('should render', () => {
+    cy.get('[data-testid="add-product-form"]').should('be.visible');
+  });
+  it('should fill in the form properly', () => {
+    const product = {
+      name: 'ElderWand',
+      price: 200,
+      currency: 'Sickle',
+      category: 'Magical artifacts',
+      packageSize: 'M',
+      tags: ['Wand', 'Magic', 'Core of gold'],
+      supportContact: 'olivander-wands@mail.wiz',
+      onSale: false,
+    };
+
+    cy.get('[name="name"]').type(product.name);
+
+    cy.get('[name="price"]').type(product.price);
+
+    cy.get('[name="currency"]').select(product.currency);
+
+    cy.get('[name=category]').select(product.category);
+
+    cy.get('[name=packageSize').check(product.packageSize);
+
+    cy.get('[name=supportContact]').type(product.supportContact);
+
+    product.tags.forEach((tag) =>
+      cy.get('[name="tags"]').type(tag).type('{enter}')
+    );
+
+    product.onSale && cy.get('[name="onSale"]').check();
+
+    cy.get('button[type="submit"]').click();
+
+    cy.get('[href="/products/all"]').click();
+
+    cy.get('[data-testid="product-card"]').contains(product.name);
+  });
+});
